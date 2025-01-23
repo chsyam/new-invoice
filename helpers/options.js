be80b8b1e5ecf624bc11af11d383992251bfc142
yogesh.b <birla.yogesh@tcs.com>
2025-01-23 10:26:33 +0000

    removed header banner from all pages

--- helpers/options.js
+++ helpers/options.js
@@ -2,17 +2,18 @@ module.exports = {
     formate: 'A4',
     orientation: 'potrait',
     border: '10mm',
+    timeout: 120000,
     header: {
-        height: '15mm',
-        contents: '<h4 style="color: red;font-size:20;font-weight:800;text-align:center;">CUSTOMER INVOICE</h4>'
+        height: '10mm',
+        contents: ``
     },
     footer: {
-        height: '20mm',
+        height: '10mm',
         contents: {
             first: 'Cover page',
             2: 'Second page',
             default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>',
             last: 'Last Page'
         }
-    }
+    },
 }
